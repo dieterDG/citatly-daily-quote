d374d415df1d588a8e8d5214590cd7dfec193f5e
Dieter Geiling <dieter@space-in.de>
2026-05-25 18:38:41 +0200

    new npm build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ff10b23ef52797a2ee77');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e00b842d2abf49b661fc');
